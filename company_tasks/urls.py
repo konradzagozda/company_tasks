fb95fbf9f1a6eef87778de8868a4cbce37f527d2
k.zagozda <k.zagozda@lekseek.com>
2022-03-23 23:13:28 +0100

    add API

--- company_tasks/urls.py
+++ company_tasks/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from tasks.views import TaskViewSet
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
