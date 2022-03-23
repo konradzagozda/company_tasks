30e6312ca6e7ce489cb523c0fc4fa7d268714060
k.zagozda <k.zagozda@lekseek.com>
2022-03-23 23:08:04 +0100

    create model, install DRF

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import PROTECT
 
-# Create your models here.
+
+class Task(models.Model):
+    TASK_CHOICES = (
+        ('DONE', 'DONE'),
+        ('TD_DO', 'TO_DO')
+    )
+
+    user = models.ForeignKey(User, on_delete=PROTECT)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    state = models.CharField(choices=TASK_CHOICES, max_length=5)
+    deadline = models.DateTimeField()
