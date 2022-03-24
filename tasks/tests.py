8e2becde03ea8c68e91b3c463d482953712ab557
k.zagozda <k.zagozda@lekseek.com>
2022-03-24 23:23:41 +0100

    create send_mail utility function

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+from tasks.utils import send_mail
+
+
+class MailTest(TestCase):
+    def test_mail(self):
+        send_mail('halo', 'halo', to_emails=['zagozdakonrad@gmail.com'])
