f46c84060865e51577173723563299ba069374c5
k.zagozda <k.zagozda@lekseek.com>
2022-03-24 21:54:59 +0100

    limit updating task for user

--- api/serializers.py
+++ api/serializers.py
@@ -10,12 +10,20 @@ class TaskSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'assigned_user', 'title', 'description', 'state', 'deadline']
 
 
+class TaskCheckSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['url', 'assigned_user', 'title', 'description', 'state', 'deadline']
+        read_only_fields = ['url', 'assigned_user', 'title', 'description', 'deadline']
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     password = serializers.CharField(write_only=True)
+    undone_tasks_no = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['url', 'username', 'password', 'email', 'groups']
+        fields = ['url', 'username', 'password', 'email', 'groups', 'undone_tasks_no']
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -24,3 +32,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
             email=validated_data['email'],
         )
         return user
+
+    def get_undone_tasks_no(self, obj):
+        return Task.objects.filter(assigned_user=obj).count()
--- api/views.py
+++ api/views.py
@@ -1,15 +1,26 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from api.permissions import UsersPermission, TasksPermission
 from tasks.models import Task
-from api.serializers import TaskSerializer, UserSerializer
+from api.serializers import TaskSerializer, UserSerializer, TaskCheckSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [TasksPermission]
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+        if self.request.user.is_superuser:
+            return serializer_class
+
+        if self.request.method in ['PUT', 'POST']:
+            serializer_class = TaskCheckSerializer
+
+        return serializer_class
 
     def get_queryset(self):
         if self.request.user.is_superuser:
