afa77f2dc3acb3c6c71247033e8e93a9ae369b77
k.zagozda <k.zagozda@lekseek.com>
2022-03-25 15:56:08 +0100

    delete email utils

--- company_tasks/settings.py
+++ company_tasks/settings.py
@@ -124,3 +124,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'oasdkoasd2123@gmail.com'
+EMAIL_HOST_PASSWORD = 'usifjisaf'
+DEFAULT_FROM_EMAIL = 'oasdkoasd2123@gmail.com'
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,8 +1,9 @@
+from django.core.mail import send_mail
 from django.test import TestCase
 
-from tasks.utils import send_mail
+from company_tasks.settings import DEFAULT_FROM_EMAIL
 
 
 class MailTest(TestCase):
     def test_mail(self):
-        send_mail('halo', 'halo', to_emails=['zagozdakonrad@gmail.com'])
+        send_mail('halo', 'halo', DEFAULT_FROM_EMAIL, ['zagozdakonrad@gmail.com'], fail_silently=False)
